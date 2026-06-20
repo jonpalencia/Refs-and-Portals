cf0d6558684ce8a6c7fcc5800ad418328970f869
jonpalencia <jonpalencia20@gmail.com>
2026-06-21 00:49:48 +0800

    add: first demo, how clunky when only using state on the input to get the value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [inputName, setInputName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChangeInput(e) {
+    setSubmitted(prev => (prev = false));
+    const nameResult = e.target.value;
+    setInputName(prevName => (prevName = nameResult));
+    console.log(nameResult);
+  }
+
+  function handleClick() {
+    setSubmitted(prev => (prev = true));
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? `${inputName}!` : 'User, '} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChangeInput} value={inputName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );

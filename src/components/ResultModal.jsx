58d2586f6c40fde562e3f21713dff2eda7ea8f88
jonpalencia <jonpalencia20@gmail.com>
2026-06-22 01:00:26 +0800

    refactor: ResultModal.jsx add score feat, tweak modal, createPortal for modal to id modal.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,16 +1,40 @@
-export default function ResultModal({ ref, result, targetTime }) {
-  return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+import React, { useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+export default function ResultModal(props) {
+  const { ref, targetTime, timeRemaining, onReset } = props;
+  const dialog = useRef();
+  const formattedTime = (timeRemaining / 1000).toFixed(2);
+  const hasLost = timeRemaining <= 0;
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+
+  useImperativeHandle(ref, () => {
+    return {
+      //* Here you can define your own callback method to be exposed and used it on the parent. As shown in the example below:
+      openModal() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      <h2>
+        {!hasLost
+          ? `🎉 Awsome! Your score: ${score}`
+          : `🚨 Time's Up! You Lose. `}
+      </h2>
       <p>
         The target time was {targetTime} second{targetTime > 1 ? 's' : ''}.
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{formattedTime} seconds left.</strong>
       </p>
       <form method="dialog">
-        <button>Close</button>
+        <button onClick={onReset}>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal'),
   );
 }
